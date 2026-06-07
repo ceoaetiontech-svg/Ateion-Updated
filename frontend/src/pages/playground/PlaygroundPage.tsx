2230b709f8c97c52954f106c762e664068199424
Aadishx07 <aadishdas7@gmail.com>
2026-06-07 12:59:13 +0530

    Add home button to playground navbar (fix invalid JSX in PlaygroundPage)

--- frontend/src/pages/playground/PlaygroundPage.tsx
+++ frontend/src/pages/playground/PlaygroundPage.tsx
@@ -1,9 +1,1 @@
 export { default } from "./layouts/PlaygroundLayout";
-import { Link } from "react-router-dom";
-import { Home } from "lucide-react";
-<Link
-  to="/"
-  className="fixed top-4 left-4 z-50 p-3 bg-white rounded-full shadow-lg"
->
-  <Home size={22} />
-</Link>
