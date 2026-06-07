30ca87250e30e021dd4b0c164ac8090c2e114d48
Yashraj Kadam <yashrajkadam555@gmail.com>
2026-06-07 12:54:34 +0530

    Update PlaygroundPage.tsx
    Add home button to playground page

--- frontend/src/pages/playground/PlaygroundPage.tsx
+++ frontend/src/pages/playground/PlaygroundPage.tsx
@@ -1,1 +1,9 @@
 export { default } from "./layouts/PlaygroundLayout";
+import { Link } from "react-router-dom";
+import { Home } from "lucide-react";
+<Link
+  to="/"
+  className="fixed top-4 left-4 z-50 p-3 bg-white rounded-full shadow-lg"
+>
+  <Home size={22} />
+</Link>
